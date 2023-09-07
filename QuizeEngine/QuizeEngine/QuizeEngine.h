d19fcbe8678d8cacb979e957a721bfd0f47fa77f
Hiren Fadadu <hirenfaldu9697@gmail.com>
2023-09-07 14:35:15 +0530

    Created 'QuizeEngine' macOS framework Added .gitignore file

---
+++ QuizeEngine/QuizeEngine/QuizeEngine.h
@@ -0,0 +1,18 @@
+//
+//  QuizeEngine.h
+//  QuizeEngine
+//
+//  Created by Hirenkumar Fadadu on 07/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuizeEngine.
+FOUNDATION_EXPORT double QuizeEngineVersionNumber;
+
+//! Project version string for QuizeEngine.
+FOUNDATION_EXPORT const unsigned char QuizeEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuizeEngine/PublicHeader.h>
+
+
