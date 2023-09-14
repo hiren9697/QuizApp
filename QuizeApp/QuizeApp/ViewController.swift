d30be84011cf1ac50ba61af93207095670c7ec0e
Hiren Fadadu <hirenfaldu9697@gmail.com>
2023-09-14 08:20:57 +0530

    Added new iOS project 'QuizeApp' Made changes to .gitignore file

---
+++ QuizeApp/QuizeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizeApp
+//
+//  Created by Hirenkumar Fadadu on 14/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
