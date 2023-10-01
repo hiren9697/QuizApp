68e00a85acbf73587a3b11ca309a844dec26a9ae
Hiren Fadadu <hirenfaldu9697@gmail.com>
2023-10-01 15:13:42 +0530

    Moved SubmitButtonController to separate file

--- QuizeApp/QuizeApp/Router/NavigationControllerRouter.swift
+++ QuizeApp/QuizeApp/Router/NavigationControllerRouter.swift
@@ -46,34 +46,6 @@ class NavigationControllerRouter: Router {
     }
 }
 
-private class SubmitButtonController: NSObject {
-    let answerCallback: ([String])-> Void
-    let button: UIBarButtonItem
-    var model: [String] = []
-    
-    init(answerCallback: @escaping ([String]) -> Void,
-         button: UIBarButtonItem) {
-        self.answerCallback = answerCallback
-        self.button = button
-        super.init()
-        setup()
-    }
-    
-    private func setup() {
-        button.action = #selector(buttonAction)
-        button.target = self
-    }
-    
-    @objc func buttonAction() {
-        answerCallback(model)
-    }
-    
-    func update(_ selection: [String]) {
-        model = selection
-        button.isEnabled = !selection.isEmpty
-    }
-}
-
 
 
 //        let questionVC = Storyboards
